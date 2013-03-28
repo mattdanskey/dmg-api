dfce2aceab5dfb5a781912a38d7775ed5d8918f4
eland <mdanskey@gmail.com>
2013-03-28 11:29:40 -0700

    Implement user authentication successfully

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, json, Response, session
+from flask import Flask, request, json, Response, session, send_from_directory
 from pymongo import MongoClient
 from bson import json_util, objectid
 from flask.ext.login import *
 
-Flask.secret_key = "p\xd2i\xe2\xf12@\x10Z\xc1^So+\xda\xf1\x01\xd6\n\xcd\xee1\xa6\x0c"
+Flask.secret_key = "NotTheKeyUsedWhenDeployed"
 
 app = Flask(__name__)
 
@@ -15,21 +15,34 @@ db = conn.test
 login_manager = LoginManager()
 login_manager.setup_app(app)
 
-
-#TODO: add authentication
 #TODO: Endpoints for user creation
 
-class User:
-  id = None
-  def get(userid):
-    user = db['users'].find_one({"_id":objectid.ObjectId(userid)})
-  def get_id():
-    return unicode(id)
-
+class User(object):
+  def __init__(self, username, userId, active=True):
+    self.username = username
+    self.userId = userId
+    self.activeStatus = active
+  
+  def is_authenticated(self):
+    return True
+    
+  def is_active(self):
+    return self.activeStatus
+    
+  def is_anonymous(self):
+    return False
+    
+  def get_id(self):
+    if self.userId is not None:
+      return unicode(self.userId)
+    else:
+      return None
 
 @login_manager.user_loader
 def load_user(userid):
-  return User.get(userid)
+  userDoc = db['users'].find_one({"_id":objectid.ObjectId(userid)})
+  getUser = User(userDoc['username'], userDoc['_id'])
+  return getUser
 
 # getting entries jsonified
 def jentries(coll, scope={}):
@@ -38,6 +51,42 @@ def jentries(coll, scope={}):
     entries["entries"].append(entry)
   return entries
 
+def loginUser(username, password):
+  userDict = db.users.find_one({'username':username})
+  if (userDict is not None) and (userDict["password"] == password):
+    user = User(userDict['username'], userDict['_id'])
+    login_user(user)
+    return True
+  else:
+    return False
+
+# Routes
+
+@app.route('/index')
+def index():
+  return send_from_directory('./', 'index.html')
+
+@app.route('/login', methods = ['POST'])
+def loginPerson():
+  if request.method == 'POST':
+    username = request.json['username']
+    password = request.json['password']
+    success = loginUser(username, password)
+    if success:
+      return 'successfully logged in'
+    else:
+      return 'error'
+    
+@app.route('/logout')
+def logoutPerson():
+  logout_user()
+  return 'logged out'
+    
+@app.route('/session')
+def put_session():
+  js = json.dumps(session)
+  resp = Response(js, status=200, mimetype='application/json')
+  return resp
 
 @app.route('/<user>/activities', methods = ['POST', 'GET'])
 @login_required
